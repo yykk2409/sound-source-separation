9803783e826c24ba26b33e7d05b82dd140622d22
yykk2409 <maetaka-2022066@edu-g.gsn.ed.jp>
2023-12-30 08:36:37 +0900

    new

--- server.py
+++ server.py
@@ -23,9 +23,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/")
+def hello_world():
+   return "<p>Hello, World!</p>"

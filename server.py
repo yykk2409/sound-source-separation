53734efcd7dd634e13ba3e87a86498bdf1e9a95b
yykk2409 <maetaka-2022066@edu-g.gsn.ed.jp>
2023-12-29 21:24:38 +0900

    a

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template, redirect
+from flask_cors import CORS
+from yt_dlp import YoutubeDL
+import demucs.separate
+app = Flask(__name__, static_folder='.', static_url_path='')
+CORS(app)
+@app.route('/')
+def index():
+    return render_template('main.html')
+@app.route('/sss',methods=["POST"])
+def sss():
+	url = request.form["url"]
+	print(url)
+	ydl_opts = {'format': 'bestaudio','outtmpl': 'sound.mp3'}
+	with YoutubeDL(ydl_opts) as ydl:
+	  ydl.download([url])
+	options = ["sound.mp3","-n","htdemucs", "--two-stems","vocals","--mp3"]
+	demucs.separate.main(options)
+	return render_template('main.html')
+app.run(port=8000, debug=True)
